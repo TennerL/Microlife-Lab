594cbd18bc918b2116d7728d6834e63ae101568e
j.klaus <j.klaus@sdnord.de>
2025-01-27 17:54:58 +0100

    implemented first example on welcome page

--- public/js/start.js
+++ public/js/start.js
@@ -1,7 +1,35 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-// var btnAspergillus = document.getElementById('btnAspergillus');
-// btnAspergillus.addEventListener('click', function(){
-//     alert("Works!");
-// })
+
+var btnSample1 = document.getElementById("btnSample1")
+let savedProjects = localStorage.getItem("savedProjects");
+
+btnSample1.addEventListener('click', function(){
+    
+    const projectData = {
+        id: "example-aspergillus",
+        projectName: "Beispiel (Aspergillus)",
+        temperature: 23,
+        concentration: 100,
+        moisture: 100,
+        microOrganism: "aspergillus",
+        countMicrobes: 10,
+        simulationTimeUnit: "d",
+        simulationTime: 2,
+        ph: 7,
+        minGermCount: 1000
+    };
+
+    if (savedProjects) {
+        savedProjects = JSON.parse(savedProjects);
+    } else {
+        savedProjects = [];
+    }
+
+    savedProjects.push(projectData);
+
+    localStorage.setItem("savedProjects", JSON.stringify(savedProjects));
+
+    window.location.href = "/main";
+})
 
 });
